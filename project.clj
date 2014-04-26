ef0a2346d20b39f7e3395677f67383cda587ae06
AlBaker <albaker@clarkparsia.com>
2014-04-25 22:48:29 -0400

    Bump deps to Starodg 2.1.3, update version to 0.3.0

--- project.clj
+++ project.clj
@@ -13,25 +13,25 @@
  ; See the License for the specific language governing permissions and
  ; limitations under the License.
 
-(defproject stardog-clj "0.1.0-SNAPSHOT"
+(defproject stardog-clj "0.3.0"
   :description "Stardog-clj: Clojure bindings for Stardog"
   :url "http://stardog.com"
   :license {:name "Apache License"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.complexible.stardog/common-rdf "2.1.2"]
-                 [com.complexible.stardog/openrdf-utils "2.1.2"]
-                 [com.complexible.stardog/shared "2.1.2"]
+                 [com.complexible.stardog/common-rdf "2.1.3"]
+                 [com.complexible.stardog/openrdf-utils "2.1.3"]
+                 [com.complexible.stardog/shared "2.1.3"]
                  [org.openrdf.sesame/sesame "2.7.10"]
-                 [com.complexible.stardog/api "2.1.2"]
-                 [com.complexible.stardog.protocols.snarl/client "2.1.2"]
-                 [com.complexible.stardog.protocols.http/client "2.1.2"]
-                 [com.complexible.stardog.reasoning.snarl/client "2.1.2"]
-                 [com.complexible.stardog.reasoning.http/client "2.1.2"]
-                 [com.complexible.stardog.search.snarl/client "2.1.2"]
-                 [com.complexible.stardog.search.http/client "2.1.2"]
-                 [com.complexible.stardog.icv.snarl/client "2.1.2"]
-                 [com.complexible.stardog.icv.http/client "2.1.2"]]
+                 [com.complexible.stardog/api "2.1.3"]
+                 [com.complexible.stardog.protocols.snarl/client "2.1.3"]
+                 [com.complexible.stardog.protocols.http/client "2.1.3"]
+                 [com.complexible.stardog.reasoning.snarl/client "2.1.3"]
+                 [com.complexible.stardog.reasoning.http/client "2.1.3"]
+                 [com.complexible.stardog.search.snarl/client "2.1.3"]
+                 [com.complexible.stardog.search.http/client "2.1.3"]
+                 [com.complexible.stardog.icv.snarl/client "2.1.3"]
+                 [com.complexible.stardog.icv.http/client "2.1.3"]]
   :plugins [[jonase/eastwood "0.0.2"]
             [lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
