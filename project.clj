a7276fc6e0354dcde83647810477d9835c67910a
Danny Freeman <daniel.freeman@stardog.com>
2023-04-05 16:59:26 -0400

    9.0.0 Released

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
 ;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
-(defproject stardog-clj "9.0.0-SNAPSHOT"
+(defproject stardog-clj "9.0.0"
   :description "Stardog-clj: Clojure bindings for Stardog"
   :url "http://stardog.com"
   :license {:name "Apache License"
